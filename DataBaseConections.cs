5025336111eefb9f3663653a2898fe7b925cde13
Сахиб Гаджиев <Altair96.96@mail.ru>
2017-05-12 16:55:22 +0300

    add metods for class DataBaseConnections

--- DataBaseConections.cs
+++ DataBaseConections.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.OracleClient;
 namespace WebCam
 {
     class DataBaseConections
     {
+        static string oradb = "Data Source=localhost:1521;Persist Security Info=True;" +
+           "User ID=myUserID;Password=1111;Unicode=True";
+        static OracleConnection connection;
+        static public bool ConnectOpen()
+        {
+            string connectionString = oradb;
+            try
+            {
+                connection.ConnectionString = connectionString;
+                connection.Open();
+            }
+            catch( Exception e)
+            {
+                Console.WriteLine("very bad");
+                return false;
+
+            }
+                
+                Console.WriteLine("State: {0}", connection.State);
+                Console.WriteLine("ConnectionString: {0}",
+                                  connection.ConnectionString);
+            return true;
+           
+        }
+        static public void ConnectClose()
+        {
+            connection.Close();
+        }
+        static bool PersoneAdd(String Name,String[] inputImage)
+        {
+
+            return true;
+        }
+        static bool PersoneDelete(String Name)
+        {
+            return true;
+        }
 
     }
 }
